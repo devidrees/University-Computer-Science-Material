d564834a92986ebf409f37d27891a0a3dc8ec9c5
devidrees <dev.idrees@gmail.com>
2024-11-11 11:07:10 +0530

    setupdone

--- CsharpApp1/obj/Debug/net8.0/App1.AssemblyInfo.cs
+++ CsharpApp1/obj/Debug/net8.0/App1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ed0038235eac4048b3c24b48507b4e6f324912")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
